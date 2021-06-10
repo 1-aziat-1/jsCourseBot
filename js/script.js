e6164029f0f297ed095cf626382827e04af79c4b
1-aziat-1 <as.ac2727@mail.ru>
2021-06-10 14:36:08 +0300

    pravki if

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@
 
 function number() {
 
-  let a = 10;
+  let a = 5;
 
   let b = prompt("Угадай число от 1 до 100");
 
   if(b<a && b != null){
-    return (alert("Загаданное число меньше"),console.log(b), number());
+    return (alert("Загаданное число меньше"), number());
   }else if(b>a){
     return (alert("Загаданное число больше"), number());
-  }else if(isNaN(b) || b.trim() === ''){
+  }else if(isNaN(b) || b && b.trim() === ''){
     return (alert("Введи число!"), number());
   }else if(b === null){
     return (alert("Игра окончена"));
