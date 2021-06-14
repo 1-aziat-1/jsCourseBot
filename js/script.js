1565c25366fec7ef6eb76d7c5c06aab593ae71da
1-aziat-1 <as.ac2727@mail.ru>
2021-06-14 19:57:13 +0300

    final

--- js/script.js
+++ js/script.js
@@ -4,22 +4,34 @@
 
 function number() {
 
-  let a = 5;
-
-  let b = prompt("Угадай число от 1 до 100");
-
-  if(b<a && b != null){
-    return (alert("Загаданное число меньше"), number());
-  }else if(b>a){
-    return (alert("Загаданное число больше"), number());
-  }else if(isNaN(b) || b && b.trim() === ''){
-    return (alert("Введи число!"), number());
-  }else if(b === null){
-    return (alert("Игра окончена"));
-  }else{
-    return (alert("Поздравляю, Вы угадали"));
+  function getRandom(){
+    return Math.floor(Math.random() *(100 - 1) + 1);
   }
 
+  let a = getRandom();
+  console.log(a);
+  chislo();
+
+  function chislo(){
+    
+    let b = prompt("Угадай число от 1 до 100");
+
+    if(b === null){
+      return (alert("Игра окончена"));
+    }else if(isNaN(b) || b.trim() === ''){
+      alert("Введи число!");
+      return   chislo();
+    }else if(b<a){
+      alert("Загаданное число больше")
+      return   chislo();
+    }else if(b>a){
+      alert("Загаданное число меньше");
+      return   chislo();
+    }else if(b === a){
+      return (alert("Поздравляю, Вы угадали"));
+    }
+    
+    }
   
 }
 
